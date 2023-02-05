e3803e3645d31069283425f2cb5ae8f976b5bf23
German David <germanmarinrolong@gmail.com>
2023-02-05 18:00:30 -0500

    update

--- lua/config/lsp/settings/sumneko_lua.lua
+++ lua/config/lsp/settings/sumneko_lua.lua
@@ -14,7 +14,7 @@ local config = {
             diagnostics = { globals = { "vim" } },
             runtime = { version = "LuaJIT" },
             telemetry = { enable = false },
-            workspace = { library = library },
+            workspace = { library = library, checkThirdParty = false },
         },
     },
 }
