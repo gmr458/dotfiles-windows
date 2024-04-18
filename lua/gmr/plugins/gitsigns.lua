5e5fac261d3e78b13180fe76155bfbcd22412c8d
German David <germanmarinrolong@gmail.com>
2024-04-18 12:11:49 -0500

    update

--- lua/gmr/plugins/gitsigns.lua
+++ lua/gmr/plugins/gitsigns.lua
@@ -2,9 +2,6 @@ return {
     'lewis6991/gitsigns.nvim',
     event = 'BufReadPost',
     config = function()
-        require('gitsigns').setup {
-            attach_to_untracked = false,
-            trouble = false,
-        }
+        require('gitsigns').setup {}
     end,
 }
