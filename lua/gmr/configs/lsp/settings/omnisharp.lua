ea87f59b9e3329f8df206faf3185de970e9acf67
German David <germanmarinrolong@gmail.com>
2024-08-02 16:26:16 -0500

    update

--- lua/gmr/configs/lsp/settings/omnisharp.lua
+++ lua/gmr/configs/lsp/settings/omnisharp.lua
@@ -1,5 +1,11 @@
+local mason = require 'mason-registry'
+local path = mason.get_package('omnisharp'):get_install_path()
+
 local config = {
-    cmd = { 'omnisharp' },
+    cmd = {
+        'dotnet',
+        path .. '/libexec/OmniSharp.dll',
+    },
 }
 
 return config
