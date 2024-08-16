c4a492ec6a006dade758bba8af799df35417cbd3
German David <germanmarinrolong@gmail.com>
2024-08-16 12:22:49 -0500

    update

--- lua/gmr/plugins/cmp.lua
+++ lua/gmr/plugins/cmp.lua
@@ -66,7 +66,7 @@ return {
             mapping = cmp.mapping.preset.insert {
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-e>'] = cmp.mapping.abort(),
+                ['<C-m>'] = cmp.mapping.abort(),
                 ['<CR>'] = cmp.mapping.confirm { select = true },
                 ['<C-Space>'] = cmp.mapping.complete(),
                 ['<Tab>'] = cmp.mapping(function(fallback)
