90780ba25287fb56d78e8bdf4fb7f27381b5d62f
German David <germanmarinrolong@gmail.com>
2024-12-11 22:54:14 -0500

    update

--- lua/gmr/plugins/base46.lua
+++ lua/gmr/plugins/base46.lua
@@ -1,7 +1,5 @@
 return {
     'NvChad/base46',
     lazy = true,
-    build = function()
-        require('base46').load_all_highlights()
-    end,
+    build = ':lua require(\'base46\').load_all_highlights()',
 }
